9c08b943ff082053d202f56b110cfd5fd34ca3f0
Wyeha <snovavzloman@mail.ru>
2023-09-05 17:23:04 +0300

    2nd done

---
+++ Example2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int CountOddPos(int[] array)
+{
+    int count = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        count+=array[i];
+    }
+    return count;
+}
+
+int[] arr = CreateArrayRndInt(6, -9, 9);
+PrintArray(arr);
+Console.WriteLine($" -> {CountOddPos(arr)}");
