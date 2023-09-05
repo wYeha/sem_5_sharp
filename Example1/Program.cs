ce723b0d9a4ae653538fb50f7d166299a7a357bf
Wyeha <snovavzloman@mail.ru>
2023-09-05 17:15:31 +0300

    1st done

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,3 +1,38 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int Count3Didgit(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] arr = CreateArrayRndInt(10, 99, 999);
+PrintArray(arr);
+Console.WriteLine($" -> {Count3Didgit(arr)}");
